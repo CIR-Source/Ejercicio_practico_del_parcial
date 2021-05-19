f90b3d35972fbf2df1493cbf7766d6b2677772dc
PC <PC@DESKTOP-OOVQ53A>
2021-05-18 20:02:11 -0500

    Primer commit del ejercicio practico del segundo parcial de programacion

---
+++ src/Examen2doParcial/Examen2doParcial.java
@@ -0,0 +1,46 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Examen2doParcial;
+
+/**
+ *
+ * @author PC
+ */
+public class Examen2doParcial {
+    
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+       videoComunicacion whatsApp = new videoComunicacion("WhatsApp");
+       whatsApp.setCantUsuarios(1000000);
+       whatsApp.setTiempo(Integer.SIZE);
+       whatsApp.setNivel("Libre");
+       whatsApp.setEstrellas(4.5);
+       
+       videoComunicacion zoom = new videoComunicacion("Zoom");
+       zoom.setCantUsuarios(5000000);
+       zoom.setTiempo(Integer.SIZE);
+       zoom.setNivel("Libre");
+       zoom.setEstrellas(4.0);
+       
+       videoComunicacion meet = new videoComunicacion("Google Meet");
+       meet.setCantUsuarios(2000000);
+       meet.setTiempo(Integer.SIZE);
+       meet.setNivel("Libre");
+       meet.setEstrellas(4.7);
+       
+       System.out.println("Los tres programas más usados para las video comunicaciones son:");
+       System.out.println("");
+       System.out.println(whatsApp);
+       System.out.println("");
+       System.out.println(zoom);
+       System.out.println("");
+       System.out.println(meet);
+    }
+    
+}
---
+++ src/Examen2doParcial/videoComunicacion.java
@@ -0,0 +1,88 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Examen2doParcial;
+
+/**
+ *
+ * @author PC
+ */
+public class videoComunicacion {
+    private String nombre;
+    private int cantUsuarios;
+    private Integer tiempo;
+    private String nivel;
+    private double estrellas;
+
+    public videoComunicacion() {}
+
+    public videoComunicacion(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getCantUsuarios() {
+        return cantUsuarios;
+    }
+
+    public void setCantUsuarios(int cantUsuarios) {
+        this.cantUsuarios = cantUsuarios;
+    }
+
+    public Integer getTiempo() {
+        return tiempo;
+    }
+
+    public void setTiempo(Integer tiempo) {
+        this.tiempo = tiempo;
+    }
+
+    public String getNivel() {
+        return nivel;
+    }
+
+    public void setNivel(String nivel) {
+        this.nivel = nivel;
+    }
+
+    public double getEstrellas() {
+        return estrellas;
+    }
+
+    public void setEstrellas(double estrellas) {
+        this.estrellas = estrellas;
+    }
+
+    public videoComunicacion(String nombre, int cantUsuarios, Integer tiempo, String nivel, double estrellas) {
+        this.nombre = nombre;
+        this.cantUsuarios = cantUsuarios;
+        this.tiempo = tiempo;
+        this.nivel = nivel;
+        this.estrellas = estrellas;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("El nombre del programa es: ").append(nombre);
+        sb.append(System.getProperty("line.separator"));
+        sb.append("Su cantidad de usuarios es: ").append(cantUsuarios);
+        sb.append(System.getProperty("line.separator"));
+        sb.append("Su tiempo es: ").append(tiempo);
+        sb.append(System.getProperty("line.separator"));
+        sb.append("Su nivel es: ").append(nivel);
+        sb.append(System.getProperty("line.separator"));
+        sb.append("Su calificación en estrellas es: ").append(estrellas);
+        return sb.toString();
+    }
+    
+}
